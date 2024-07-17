f218231385af6d87b2eef81b632b2eec2f5c7187
LesyaKot <lesyakot83@gmail.com>
2024-07-17 12:00:49 +0300

    add get/post/patch/delete.yaml

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -25,10 +26,13 @@ const setupServer = () => {
 
   app.use('/', router);
 
+  app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerDocs());
+
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
-  app.use('/uploads', express.static(UPLOAD_DIR));
 
    app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
