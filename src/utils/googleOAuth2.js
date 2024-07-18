a28e140d75923607514126f2a181d7a8bee1bf68
LesyaKot <lesyakot83@gmail.com>
2024-07-18 10:50:58 +0300

    fix oauth2

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -1,11 +1,15 @@
 import { OAuth2Client } from 'google-auth-library';
-import oauthConfig from '../../google-oauth.json';
+import path from 'node:path';
+import { readFile } from 'fs/promises';
 import createHttpError from 'http-errors';
-import { ENV_VARS, env } from './env.ts';
+
+const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
+
+const oauthConfig = JSON.parse(await readFile(PATH_JSON, 'utf8'));
 
 const googleOAuthClient = new OAuth2Client({
-  clientId: env(ENV_VARS.GOOGLE_AUTH_CLIENT_ID),
-  clientSecret: env(ENV_VARS.GOOGLE_AUTH_CLIENT_SECRET),
+  clientId: process.env.GOOGLE_AUTH_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
   redirectUri: oauthConfig.web.redirect_uris[0],
 });
 
@@ -17,22 +21,23 @@ export const generateAuthUrl = () =>
     ],
   });
 
-export const validateCode = async (code) => {
-  const response = await googleOAuthClient.getToken(code);
-  if (!response.tokens.id_token) throw createHttpError(401, 'Unauthorized');
-  const ticket = await googleOAuthClient.verifyIdToken({
-    idToken: response.tokens.id_token,
-  });
-  return ticket;
-};
+  export const validateCode = async (code) => {
+    const response = await googleOAuthClient.getToken(code);
+    if (!response.tokens.id_token) throw createHttpError(401, 'Unauthorized');
 
-export const getFullNameFromGoogleTokenPayload = (payload) => {
-  let fullName = 'Guest';
-  if (payload.given_name && payload.family_name) {
-    fullName = `${payload.given_name} ${payload.family_name}`;
-  } else if (payload.given_name) {
-    fullName = payload.given_name;
-  }
+    const ticket = await googleOAuthClient.verifyIdToken({
+      idToken: response.tokens.id_token,
+    });
+    return ticket;
+  };
 
-  return fullName;
-};
+  export const getFullNameFromGoogleTokenPayload = (payload) => {
+    let fullName = 'Guest';
+    if (payload.given_name && payload.family_name) {
+      fullName = `${payload.given_name} ${payload.family_name}`;
+    } else if (payload.given_name) {
+      fullName = payload.given_name;
+    }
+
+    return fullName;
+  };
